3c68b06611a25aa0faa2384d3ef0a98f00eee678
Thomas Burton <tburton5572@gmail.com>
2021-10-21 11:33:49 +0100

    FFT on vector field works

--- src/Fields.jl
+++ src/Fields.jl
@@ -6,7 +6,6 @@ include("spectralfield.jl")
 include("vectorfields.jl")
 include("fft.jl")
 
-# TODO: Test for FFT of vector field
 # TODO: Performance of FFT for different plans and sizes
 # TODO: generate weights for inner products and norms (Inside FGGrids.jl package)
 
--- test/test_fft.jl
+++ test/test_fft.jl
@@ -10,15 +10,27 @@
                 rand(Float64, (Ny, Ny)),
                 rand(Float64, (Ny, Ny)),
                 rand(Float64, Ny))
-    A = PhysicalField(grid); Â = SpectralField(grid); B = PhysicalField(grid)
-    A.data .= rand(Float64, (Ny, Nz, Nt))
+    A1 = PhysicalField(grid); Â1 = SpectralField(grid); B1 = PhysicalField(grid)
+    A2 = PhysicalField(grid); Â2 = SpectralField(grid); B2 = PhysicalField(grid)
+    A1.data .= rand(Float64, (Ny, Nz, Nt))
+    A2.data .= rand(Float64, (Ny, Nz, Nt))
+
+    # initialise vector field
+    𝐀 = VectorField(A1, A2)
+    𝐀̂ = VectorField(Â1, Â2)
+    𝐁 = VectorField(B1, B2)
 
     # create plans
-    FFTplan = FFTPlan!(A, flags = FFTW.ESTIMATE)
-    IFFTplan = IFFTPlan!(Â, flags = FFTW.ESTIMATE)
+    FFTplan = FFTPlan!(A1, flags = FFTW.ESTIMATE)
+    IFFTplan = IFFTPlan!(Â1, flags = FFTW.ESTIMATE)
 
     # is the transform invertible correctly
-    FFTplan(Â, A)
-    IFFTplan(B, Â)
-    @test A ≈ B
+    FFTplan(Â1, A1)
+    IFFTplan(B1, Â1)
+    FFTplan(𝐀̂, 𝐀)
+    IFFTplan(𝐁, 𝐀̂)
+    @test A1 ≈ B1
+    for i in 1:2
+        @test 𝐀[i] ≈ 𝐁[i]
+    end
 end
