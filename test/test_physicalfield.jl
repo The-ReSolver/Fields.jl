d699ecbf6ac77eb44fca12f66c40a7ee478422eb
Thomas Burton <tburton5572@gmail.com>
2022-10-26 11:49:35 +0100

    intermediate commit

--- test/test_physicalfield.jl
+++ test/test_physicalfield.jl
@@ -1,8 +1,6 @@
 @testset "Physical Field Constructor    " begin
         # take random variables
-        Ny = rand(3:50)
-        Nz = rand(3:50)
-        Nt = rand(3:50)
+        Ny = rand(3:50); Nz = rand(3:50); Nt = rand(3:50)
         y = rand(Float64, Ny)
         Dy = rand(Float64, (Ny, Ny))
         Dy2 = rand(Float64, (Ny, Ny))
@@ -15,17 +13,19 @@
         @test isa(PhysicalField(grid), PhysicalField{Ny, Nz, Nt, typeof(grid), Float64, Array{Float64, 3}})
 
         # construct from function
-        fun(y, z, t) = (1 - y^2)
-        y = range(-1, stop=1, length=11)
-        f = PhysicalField(Grid(collect(y), 1, 1, rand(11, 11), rand(11, 11), rand(11), ω, β), fun)
-        @test vec(parent(f)) == fun.(y, 0, 0)
+        fun(y, z, t) = (1 - y^2)*exp(cos(z))*atan(sin(t))
+        f = PhysicalField(grid, fun)
+        y, z, t = points(grid)
+        out = zeros(Ny, Nz, Nt)
+        for nt = 1:Nt, nz in 1:Nz, ny in 1:Ny
+                out[ny, nz, nt] = fun(y[ny], z[nz], t[nt])
+        end
+        @test f == out
 end
 
 @testset "Physical Field Broadcasting   " begin
         # take random variables
-        Ny = rand(3:50)
-        Nz = rand(3:50)
-        Nt = rand(3:50)
+        Ny = rand(3:50); Nz = rand(3:50); Nt = rand(3:50)
         ω = abs(randn())
         β = abs(randn())
 
