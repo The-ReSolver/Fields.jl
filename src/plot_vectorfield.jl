4036dc1c037fbec0bc89f6fbf81a9059f6a7afe9
Thomas Burton <tburton5572@gmail.com>
2023-04-13 12:27:18 +0100

    some plotting tweaks

--- src/plot_vectorfield.jl
+++ src/plot_vectorfield.jl
@@ -7,34 +7,42 @@
 @recipe function f(p::FieldPlot; arrow_scale=0.025)
     # unpack the arguments
     field, ti = p.args
-    y, z, _ = points(get_grid(field))
+    y, z, _ = points(get_grid(field)); z = collect(z)
+
+    # append field for periodicity
+    extended_field = Vector{typeof(parent((field[1])))}(undef, length(field))
+    for i in eachindex(field)
+        extended_field[i] = cat(parent(field[i]), field[i][:, 1:1, :]; dims=2)
+    end
+    push!(z, 2π)
 
     # setup the plot
     legend := false
     framestyle := :axes
     xlabel := "\$z\$"
     ylabel := "\$y\$"
-    font_size --> 20
     grid := false
     xlims := (0, 2π)
     ylims := (-1, 1)
     colorbar := :right
     colorbar_title --> "\$u^\\prime\$"
-    # colorbar_titlefontsize := 15
     colorbar_titlefontvalign := :vcenter
 
     # contour plot
     @series begin
-        seriestype := :contour
-        z, y, field[1][:, :, ti]
+        seriestype := :contourf
+        color --> :jet
+        levels --> 40
+        z, y, extended_field[1][:, :, ti]
     end
 
     # quiver plot
     @series begin
-        V = @view(field[2][:, :, ti]); W = @view(field[3][:, :, ti])
+        V = @view(extended_field[2][:, :, ti]); W = @view(extended_field[3][:, :, ti])
         quiver := arrow_scale .* (V[:], W[:])
         seriestype := :quiver
         linecolor --> :black
+        arrow --> (:closed, :head, (0.2, 0.2)) # ! ARROWHEAD SIZE SEEMS TO NOT BE SUPPORTED IN GR
         repeat(z, inner=length(y)), repeat(y, outer=length(z))
     end
 end
