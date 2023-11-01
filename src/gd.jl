0ef3cd2d161c1ee35ab254b3e6edc28f05bdce89
Thomas Burton <tburton5572@gmail.com>
2023-11-01 13:58:00 +0000

    gradient descent now modifies input argument

--- src/gd.jl
+++ src/gd.jl
@@ -1,15 +1,15 @@
 # This file contains the methods needed to perform a basic gradient descent
 # optimisation of a field.
 
-function gd(u₀::VectorField{3, SpectralField}, modes::Array{ComplexF64, 4}, Re::Real, Ro::Real; fix_mean::Bool=true, eps::Float64=1e-3, α::Float64=1e-3, maxiter::Int=1000)
+function gd!(u::VectorField{3, SpectralField}, modes::Array{ComplexF64, 4}, Re::Real, Ro::Real; fix_mean::Bool=true, eps::Float64=1e-3, α::Float64=1e-3, maxiter::Int=1000)
     # initialise the gradient function
-    dR! = ResGrad(u₀.grid, modes, u[:, 1, 1], Re, Ro, fix_mean=fix_mean)
+    dR! = ResGrad(u.grid, modes, u[:, 1, 1], Re, Ro, fix_mean=fix_mean)
 
     # obtain the projected velocity coefficients
     a = similar(dR!.out)
-    project!(a[1], u₀[1], u₀.grid.ws, @view(modes[:, 1:Ny, :, :]))
-    project!(a[2], u₀[2], u₀.grid.ws, @view(modes[:, (Ny + 1):2*Ny, :, :]))
-    project!(a[3], u₀[3], u₀.grid.ws, @view(modes[:, (2*Ny + 1):3*Ny, :, :]))
+    project!(a[1], u[1], u.grid.ws, @view(modes[:, 1:Ny, :, :]))
+    project!(a[2], u[2], u.grid.ws, @view(modes[:, (Ny + 1):2*Ny, :, :]))
+    project!(a[3], u[3], u.grid.ws, @view(modes[:, (2*Ny + 1):3*Ny, :, :]))
 
     # loop to step in descent direction
     i = 0
@@ -33,7 +33,6 @@ function gd(u₀::VectorField{3, SpectralField}, modes::Array{ComplexF64, 4}, Re
     end
 
     # convert final result back to full-space
-    u = similar(u₀)
     u[1] .= dR!.spec_cache[1]
     u[2] .= dR!.spec_cache[2]
     u[3] .= dR!.spec_cache[3]
