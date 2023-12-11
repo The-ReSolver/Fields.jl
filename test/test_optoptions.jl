6d633fc78e5cc136c201960ffe56812f6a3b42b6
Thomas Burton <tburton5572@gmail.com>
2023-12-11 14:24:58 +0000

    fixed options tests for changes to callback

--- test/test_optoptions.jl
+++ test/test_optoptions.jl
@@ -2,6 +2,6 @@
     @test OptOptions().write_loc == "./"
     @test_throws AssertionError OptOptions(; write_loc="/home/tom/somewhere")
     @test OptOptions(; write_loc="/home/tom/somewhere/").write_loc == "/home/tom/somewhere/"
-    @test OptOptions().callback isa Callback{false}
-    @test OptOptions(; callback=Callback(; write=true)).callback isa Callback{true}
+    @test !OptOptions().callback.write
+    @test OptOptions(; callback=Callback(; write=true)).callback.write
 end
