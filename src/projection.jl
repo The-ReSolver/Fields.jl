e0d1cb6af08bddcb34919327c04a18c9bb589fbe
Thomas Burton <tburton5572@gmail.com>
2024-10-05 22:00:44 +0100

    removed unnecessary projection methods

--- src/projection.jl
+++ src/projection.jl
@@ -7,31 +7,6 @@
 """
 channel_int(u::AbstractVector{<:Number}, w::AbstractVector{<:Number}, v::AbstractVector{<:Number}) = sum(w[i]*dot(u[i], v[i]) for i in eachindex(u))
 
-"""
-    Project a channel profile onto a set of modes, returning the projected
-    profile.
-"""
-function project!(a::AbstractVector{<:Number}, u::AbstractVector{<:Number}, w::AbstractVector{<:Number}, modes::AbstractMatrix{<:Number})
-    for i in axes(modes, 2)
-        a[i] = channel_int(@view(modes[:, i]), w, u)
-    end
-
-    return a
-end
-project(u::AbstractVector{<:Number}, w::AbstractVector{<:Number}, modes::AbstractMatrix{T}) where {T<:Number} = project!(zeros(T, size(modes, 2)), u, w, modes)
-
-"""
-    Project a spectral field onto a set of modes, returning the projected field
-"""
-function project!(a::AbstractArray{<:Number, 3}, u::AbstractArray{<:Number, 3}, w::AbstractVector{<:Number}, modes::AbstractArray{<:Number, 4})
-    for I in CartesianIndices(@view(a[1, [Colon() for _ in 1:(ndims(a) - 1)]...]))
-        project!(@view(a[:, I]), @view(u[:, I]), w, @view(modes[:, :, I]))
-    end
-
-    return a
-end
-project(u::AbstractArray{<:Number, 3}, w::AbstractVector{<:Number}, modes::AbstractArray{T, 4}) where {T<:Number} = project!(zeros(T, size(modes, 2), size(selectdim(u, 1, 1))...), u, w, modes)
-
 # TODO: specialiase these methods for spectral fields
 """
     Project a vector field onto a set of modes, returning the projected field
@@ -49,15 +24,6 @@ function project!(a::AbstractArray{T, 3}, u::AbstractVector{<:AbstractArray{<:Nu
     return a
 end
 project(u::AbstractVector{<:AbstractArray{<:Number, 3}}, w::AbstractVector{<:Number}, modes::AbstractArray{T, 4}) where {T<:Number} = project!(zeros(T, size(modes, 2), size(selectdim(u[1], 1, 1))...), u, w, modes)
-
-# TODO: remove this wasteman
-function reverse_project!(u::AbstractArray{<:Number, 3}, a::AbstractArray{<:Number, 3}, modes::AbstractArray{<:Number, 4})
-    for I in CartesianIndices(@view(u[1, [Colon() for _ in 1:(ndims(a) - 1)]...]))
-        mul!(@view(u[:, I]), @view(modes[:, :, I]), @view(a[:, I]))
-    end
-
-    return u
-end
 
 function expand!(u::AbstractVector{<:AbstractArray{T, 3}}, a::AbstractArray{<:Number, 3}, modes::AbstractArray{<:Number, 4}) where {T}
     N = Int(size(modes, 1)/length(u))
--- test/test_projection.jl
+++ test/test_projection.jl
@@ -9,40 +9,6 @@
     @test Fields.channel_int(u, ws, v) ≈ 0.0530025 rtol=1e-6
 end
 
-@testset "Projection Onto Mode Set              " begin
-    # construct a set of modes
-    N = 64
-    M = rand(1:12)
-    ws = ones(N)
-    Ψ = @view(qr(rand(ComplexF64, N, M)).Q[:, 1:M])
-
-    # generate channel profile as combination of modes
-    a = rand(ComplexF64, M)
-    u = Ψ*a
-
-    @test project(u, ws, Ψ) ≈ a
-end
-
-@testset "Projection of Field                   " begin
-    # construct a field of modes
-    Ny = 16; Nz = 16; Nt = 16
-    M = rand(1:12)
-    ws = ones(Ny)
-    Ψ = zeros(ComplexF64, Ny, M, Nz, Nt)
-    for nt in 1:Nt, nz in 1:Nz
-        Ψ[:, :, nz, nt] .= @view(qr(rand(ComplexF64, Ny, M)).Q[:, 1:M])
-    end
-
-    # generate field as combination of modes
-    a = rand(ComplexF64, M, Nz, Nt)
-    u = zeros(ComplexF64, Ny, Nz, Nt)
-    for nt in 1:Nt, nz in 1:Nz
-        u[:, nz, nt] .= Ψ[:, :, nz, nt]*a[:, nz, nt]
-    end
-
-    @test project(u, ws, Ψ) ≈ a
-end
-
 @testset "Projection of Vector Field            " begin
     # construct a field of modes
     Ny = 16; Nz = 16; Nt = 16
@@ -65,25 +31,28 @@ end
     @test project(u, ws, Ψ) ≈ a
 end
 
-@testset "Reverse Projection of a Field         " begin
+@testset "Expansion of a Field                  " begin
     # construct the field of modes
     Ny = 16; Nz = 16; Nt = 16
     M = rand(1:12)
     ws = ones(Ny)
-    Ψ = zeros(ComplexF64, Ny, M, Nz, Nt)
+    Ψ = zeros(ComplexF64, 3*Ny, M, Nz, Nt)
     for nt in 1:Nt, nz in 1:Nz
-        Ψ[:, :, nz, nt] .= @view(qr(rand(ComplexF64, Ny, M)).Q[:, 1:M])
+        Ψ[:, :, nz, nt] .= @view(qr(rand(ComplexF64, 3*Ny, M)).Q[:, 1:M])
     end
 
     # construct field as a combination of the modes
     a = rand(ComplexF64, M, Nz, Nt)
-    u = zeros(ComplexF64, Ny, Nz, Nt)
+    u = [zeros(ComplexF64, Ny, Nz, Nt) for i in 1:3]
+    v = [zeros(ComplexF64, Ny, Nz, Nt) for i in 1:3]
     for nt in 1:Nt, nz in 1:Nz
-        u[:, nz, nt] .= Ψ[:, :, nz, nt]*a[:, nz, nt]
+        u[1][:, nz, nt] .= Ψ[1:Ny, :, nz, nt]*a[:, nz, nt]
+        u[2][:, nz, nt] .= Ψ[Ny+1:2*Ny, :, nz, nt]*a[:, nz, nt]
+        u[3][:, nz, nt] .= Ψ[2*Ny+1:3*Ny, :, nz, nt]*a[:, nz, nt]
     end
 
     # project and reverse to get the original field
-    v = reverse_project!(similar(u), project(u, ws, Ψ), Ψ)
+    expand!(v, project(u, ws, Ψ), Ψ)
 
     @test v ≈ u
 end
