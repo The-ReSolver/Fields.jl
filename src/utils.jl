c7f0febded5eab679dd80152eda8436f7606696b
Thomas Burton <tburton5572@gmail.com>
2024-10-16 21:45:32 +0100

    fixed bug in energy of vector field

--- src/utils.jl
+++ src/utils.jl
@@ -19,7 +19,7 @@ energy(p::PhysicalField{<:Grid{Ny, Nz, Nt}}) where {Ny, Nz, Nt} = energy!(zeros(
 
 function energy(u::VectorField{N, S}) where {N, Ny, Nz, Nt, S<:SpectralField{<:Grid{Ny, Nz, Nt}}}
     K = zeros(Nt)
-    up = [PhysicalField{false}(get_grid(u), brfft(parent(u[i]), Nz, [2, 3])) for i in 1:N]
+    up = [PhysicalField{false, 1.0}(get_grid(u), brfft(parent(u[i]), Nz, [2, 3])) for i in 1:N]
     for i in 1:N
         K .+= energy(up[i])
     end
